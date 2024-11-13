e31972e67f18207f67336eab2d40a734c4ff761e
RichoKD <jerrykaduma@gmail.com>
2024-11-13 09:35:43 +0100

    Fix: Fib count

--- src/week_two/day_four.rs
+++ src/week_two/day_four.rs
@@ -17,7 +17,7 @@ pub fn generate_fibonacci_num(n: u32) -> u32 {
 
     let mut a = 0;
     let mut b = 1;
-    for _ in 2..=n {
+    for _ in 2..n {
         let temp = a + b;
         a = b;
         b = temp;
